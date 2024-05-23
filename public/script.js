49b979b2ecf11c88859ff8c6fffb6f673825ff30
Nadamohd <nmbotaibi@sm.imamu.edu.sa>
2024-05-24 02:31:49 +0300

    modify

--- public/script.js
+++ public/script.js
@@ -1,6 +1,4 @@
 //search bar
-
-
 const places = [
   { name: 'Half Million', type: 'Cafe', location: 'Riyadh', rating: 4.9 },
   { name: 'Urth', type: 'Cafe', location: 'Riyadh', rating: 4.8 },
@@ -37,27 +35,7 @@ function filterResults() {
     resultsContainer.innerHTML = '<p>No results found</p>';
   }
 }
-
 // end of search bar
-
-
-// alert message in login
-
-function validateForm() {
-  // Get the email and password inputs
-  var emailInput = document.querySelector('.signinForm .email input');
-  var passwordInput = document.querySelector('.signinForm .password input');
-
-  // Check if email and password are correct (dummy validation for demonstration) //احس يبي لنا نعدل عليها بعدين
-  if (emailInput.value !== 'Example@Example.Com' || passwordInput.value !== 'Password') {
-    // If incorrect, show alert and prevent form submission
-    alert('Incorrect email or password');
-    return false; // Prevent form submission
-  }
-  S
-  // If correct, allow form submission
-  return true;
-}
 
 //loader//
 function HomePageloader(){
